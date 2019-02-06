6203c0c2bd3afc734a669f883a2fec58c62a9747
L7907661 <hlbtaioe@163.com>
2019-02-06 16:00:26 -0500

    added teammates'names

--- src/rosebot.py
+++ src/rosebot.py
@@ -6,7 +6,7 @@
     -- Lower-level code to interact with the EV3 robot library.
 
   Author:  Your professors (for the framework and lower-level code)
-    and PUT_YOUR_NAMES_HERE.
+    and Zeyu Liao, Josiah Hasegawa, Yifan Dai.
   Winter term, 2018-2019.
 """
 
