c2070a7add2ee6ed28213dd1353a3d97a8884f68
L7907661 <hlbtaioe@163.com>
2019-02-06 15:51:20 -0500

    added name

--- src/m2_run_this_on_laptop.py
+++ src/m2_run_this_on_laptop.py
@@ -3,7 +3,7 @@
   Displays the Graphical User Interface (GUI) and communicates with the robot.
 
   Authors:  Your professors (for the framework)
-    and Zeyu Liao.
+    and Zeyu Liao (.
   Winter term, 2018-2019.
 """
 
