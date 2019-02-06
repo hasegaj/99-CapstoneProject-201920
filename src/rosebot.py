629b51d44e3c13c1092a6f1dd103049cbe4227ea
hasegaj <hasegaj@rose-hulman.edu>
2019-02-06 16:39:34 -0500

    change calibrate_arm(self): (this time is <=)

--- src/rosebot.py
+++ src/rosebot.py
@@ -211,7 +211,7 @@ class ArmAndClaw(object):
         self.motor.reset_position()
         self.motor.turn_on(-100)
         while True:
-            if abs(self.motor.get_position()) >= 14.2*360:
+            if abs(self.motor.get_position()) <= 14.2*360:
                 self.motor.turn_off()
                 self.motor.reset_position()
                 break
@@ -238,7 +238,7 @@ class ArmAndClaw(object):
         """
         self.motor.turn_on(-100)
         while True:
-            if abs(self.motor.get_position()) == 14.2*360:
+            if abs(self.motor.get_position()) >= 14.2*360:
                 self.motor.turn_off()
                 self.motor.reset_position()
                 break
