4501938a6d91491988e86475c39145ccd234e708
L7907661 <hlbtaioe@163.com>
2019-02-11 16:48:53 -0500

    test

--- src/shared_gui_delegate_on_robot.py
+++ src/shared_gui_delegate_on_robot.py
@@ -11,12 +11,15 @@ class Reciver(object):
         'Type hint: robot: rosebot.RoseBot'
         self.robot = robot
     def forward(self, left_wheel_speed, right_wheel_speed):
-        print('GO forward',left_wheel_speed, right_wheel_speed)
+        print('forward',left_wheel_speed, right_wheel_speed)
         self.robot.drive_systems.go(int(left_wheel_speed),int(right_wheel_speed))
     def quit(self):
-        print('got quit')
+        print('quit')
         self.is_time_to_stop = True
     def beep(self, n):
         print('got beep')
         for _ in range(int(n)):
             self.robot.sound_system.beeper.beep().wait()
+    def stop(self):
+        self.robot.drive_systems.go(0,0)
+
