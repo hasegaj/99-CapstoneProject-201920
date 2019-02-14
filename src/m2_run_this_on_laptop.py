9ce06e349668937491be5d861711c25388aff9e2
L7907661 <hlbtaioe@163.com>
2019-02-14 14:53:22 -0500

    add new GUI

--- src/m2_run_this_on_laptop.py
+++ src/m2_run_this_on_laptop.py
@@ -13,8 +13,8 @@ from tkinter import ttk
 import shared_gui
 
 
-def main():
 
+def main():
     """
     This code, which must run on a LAPTOP:
       1. Constructs a GUI for my part of the Capstone Project.
@@ -23,8 +23,6 @@ def main():
     # -------------------------------------------------------------------------
     # Construct and connect the MQTT Client:
     # -------------------------------------------------------------------------
-
-
     mqtt_sender = com.MqttClient()
     mqtt_sender.connect_to_ev3()
 
@@ -32,27 +30,26 @@ def main():
     # The root TK object for the GUI:
     # -------------------------------------------------------------------------
     root = tkinter.Tk()
-    root.title("CSSE 120 Winter 2018")
+    root.title("CSSE120 Capstone Project, Winter 2018-19")
 
     # -------------------------------------------------------------------------
     # The main frame, upon which the other frames are placed.
     # -------------------------------------------------------------------------
-    main_frame = ttk.Frame(root, padding=10, borderwidth=5, relief="groove")
-
-
+    main_frame = ttk.Frame(root, padding = 10, borderwidth = 5, relief = "groove")
+    main_frame.grid()
     # -------------------------------------------------------------------------
     # Sub-frames for the shared GUI that the team developed:
     # -------------------------------------------------------------------------
-    main_frame.grid()
+    teleop_frame, arm_frame,control_frame, sound_frame, drive_frame = get_shared_frames(main_frame,mqtt_sender)
     # -------------------------------------------------------------------------
     # Frames that are particular to my individual contributions to the project.
     # -------------------------------------------------------------------------
-    # done: Implement and call get_my_frames(...)
-    teleop_frame, arm_frame, control_frame = get_shared_frames(main_frame, mqtt_sender)
+    # TODO: Implement and call get_my_frames(...)
+
     # -------------------------------------------------------------------------
     # Grid the frames.
     # -------------------------------------------------------------------------
-    grid_frames(teleop_frame,arm_frame,control_frame)
+    grid_frames(teleop_frame, arm_frame, control_frame, sound_frame, drive_frame)
 
     # -------------------------------------------------------------------------
     # The event loop:
@@ -61,18 +58,19 @@ def main():
 
 
 def get_shared_frames(main_frame, mqtt_sender):
-    teleop_frame = shared_gui.get_teleoperation_frame(main_frame, mqtt_sender)
+    teleop_frame = shared_gui.get_teleoperation_frame(main_frame , mqtt_sender)
     arm_frame = shared_gui.get_arm_frame(main_frame, mqtt_sender)
-    control_frame = shared_gui.get_control_frame(main_frame, mqtt_sender)
-    return teleop_frame,arm_frame,control_frame
+    control_fram = shared_gui.get_control_frame(main_frame,mqtt_sender)
+    sound_frame = shared_gui.get_soundsystem_frame(main_frame,mqtt_sender)
+    drive_frame = shared_gui.get_drivesystem_frame(main_frame,mqtt_sender)
+    return teleop_frame, arm_frame, control_fram, sound_frame, drive_frame
 
-
-
-def grid_frames(teleop_frame, arm_frame, control_frame):
-    teleop_frame.grid(row=0, column=0)
-    arm_frame.grid(row=1, column=0)
-    control_frame.grid(row=2, column=0)
-
+def grid_frames(teleop_frame, arm_frame, control_frame, sound_frame, drive_frame):
+    teleop_frame.grid(row= 0, column = 0)
+    arm_frame.grid(row= 0, column = 1)
+    control_frame.grid(row= 1, column = 1)
+    sound_frame.grid(row= 3, column = 0)
+    drive_frame.grid(row= 3, column = 1)
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
